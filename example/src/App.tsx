117ad3f7476b4cd7333ff8cb4c5cfd7a0bac1d7d
Didier Gonzalez <alandidiergon@gmail.com>
2026-01-26 18:31:32 -0600

    preview

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,40 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-echarts-wrapper';
+import { Echarts } from 'react-native-echarts-wrapper';
 
-const result = multiply(3, 7);
 
 export default function App() {
+  const pieOption = {
+    series: [
+      {
+        name: "Source",
+        type: "pie",
+        legendHoverLink: true,
+        hoverAnimation: true,
+        avoidLabelOverlap: true,
+        startAngle: 180,
+        radius: "55%",
+        center: ["50%", "35%"],
+        data: [
+          { value: 105.2, name: "android" },
+          { value: 310, name: "iOS" },
+          { value: 234, name: "web" },
+        ],
+        label: {
+          normal: {
+            show: true,
+            textStyle: {
+              fontSize: 12,
+              color: "#23273C",
+            },
+          },
+        },
+      },
+    ],
+  };
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+          <View style={{ height: 300, paddingTop: 25 }}>
+            <Echarts height={250} option={pieOption} />
+          </View>
   );
 }
 
